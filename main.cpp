97362e035c5c243df7b725e8f7fb13ffd13e5383
Abhishek Sinha <email4abhi@gmail.com>
2021-09-27 11:46:13 +0530

    Added POST /signup which takes Username & Password.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "inc/crow_all.h"
 
 #include<iostream>
+#include<string>
 
 using namespace std;
 
@@ -11,9 +12,27 @@ int main()
 
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
+    CROW_ROUTE(app, "/")
+    ([](){
         return "Hello world";
     });
 
-    app.port(18080).run();
+    CROW_ROUTE(app, "/signup").methods(crow::HTTPMethod::GET, crow::HTTPMethod::POST)
+    ([](const crow::request& req){
+
+        auto recvd_json = crow::json::load(req.body);
+        if (!recvd_json)
+            return crow::response(400);
+
+        //username
+        const string username =  recvd_json["username"].s();
+        //password
+        const string password = recvd_json["password"].s();
+
+        
+        string reponse_json = "Hello " + username + ", your password is :: " + password;
+        return crow::response(reponse_json);//"Hello " + ", your password is :: " ;
+    });
+
+    app.port(18080).multithreaded().run();
 }
